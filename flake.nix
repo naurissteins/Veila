8046ed285c6c328df4c3c22a61f3da547df9ea31
Nauris Steins <me@naurissteins.com>
2026-05-25 08:47:18 +0300

    build: bump version to 0.4.1

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         rec {
           veila = pkgs.rustPlatform.buildRustPackage {
             pname = "veila";
-            version = "0.4.0";
+            version = "0.4.1";
 
             src = self;
 
